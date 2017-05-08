7bde895388b643a3051924378ca691236df9428a
Da Teng <da_teng0702@hotmail.com>
2017-05-07 17:22:56 -0700

    marcador migrated database

---
+++ README.md
@@ -0,0 +1,7 @@
+# First touch of Django
+
+Just some random projects as the by-product of Django learning process.
+
+[![License: CC BY 4.0](https://img.shields.io/badge/License-CC%20BY%204.0-lightgrey.svg)](http://creativecommons.org/licenses/by/4.0/)
+
+This work used the material from the famous [django-marcador](http://django-marcador.keimlink.de/en/index.html) Django tutorial created by Markus Zapke-Gründemann et al.
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,8 +11,10 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
 import os
 
+# @@@ project directory
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -37,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'marcador',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -52,10 +55,13 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'mysite.urls'
 
+
+# @@@ Template configuration
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # Configure where the template will be located
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -100,3 +106,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
